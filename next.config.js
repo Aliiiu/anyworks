63f6f314e0e03b8a3cad4276951962e907fa5cd0
dammieayokanmi <dammieayokanmi@gmail.com>
2022-07-12 02:05:23 +0100

    UPDATE next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
 
-module.exports = nextConfig
+module.exports = withPlugins([withImages], {
+  reactStrictMode: true,
+  // publicRuntimeConfig: {
+  //   // Will be available on both server and client
+  //   APP_NAME: process.env.APP_NAME,
+  // },
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/services': { page: '/services' },
+      '/faq': { page: '/faq' },
+      '/contact-us': { page: '/contact-us' },
+      '/privacy-policy': { page: '/privacy-policy' },
+      '/terms-of-use': { page: '/terms-of-use' },
+    }
+  },
+})
