baf3db9ee38491a787c7bd94eee5e085014903f0
dammieayokanmi <dammieayokanmi@gmail.com>
2022-07-28 10:51:24 +0100

    UPDATE: joinwaitlist component style

--- components/section/Joinwaitlist.tsx
+++ components/section/Joinwaitlist.tsx
@@ -6,10 +6,10 @@ const Joinwaitlist = () => {
 		<div id='waitlist' className='xl:scroll-mt-[40rem] scroll-mt-[10rem]'>
 			<div className='xl:mt-[300px] mt-[100px] relative bg-[#7E00C4] flex rounded-[16px] xl:rounded-[48px] p-[30px] md:[40px] lg:pl-[60px] lg:pr-0 lg:py-[70px]'>
 				<div className='xl:w-[65%] lg:w-[90%] w-full'>
-					<h1 className='text-white mb-[15px] capitalize text-[28px] lg:text-[40px] text-center leading-[30px] lg:leading-[45px] lg:text-left '>
+					<h1 className='text-white mb-[15px] capitalize text-[28px] lg:text-[40px] leading-[30px] lg:leading-[45px] lg:text-left '>
 						Sending Verified Artisans to Your Convenience
 					</h1>
-					<p className='text-white mb-[23px] text-center text-sm lg:text-left w-full lg:w-[95%] lg:mx-[inherit] mx-[auto]'>
+					<p className='text-white mb-[23px] text-sm lg:text-left w-full lg:w-[95%] lg:mx-[inherit] mx-[auto]'>
 						Locating trusted and proficient artisans around your vicinity can be
 						extremely difficult to do on your own. Anyworks helps you to
 						eliminate such tension by connecting you with a proficient artisan
