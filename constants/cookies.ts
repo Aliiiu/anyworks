87c55546c8b67816554db04576779cb306720216
Aliu Salaudeen <ayokunumi99@gmail.com>
2022-10-20 11:20:53 +0100

    UPDATE: privacy page updated

---
+++ constants/cookies.ts
@@ -0,0 +1,31 @@
+const cookies = [
+	{
+		id: 1,
+		type: 'Strictly necessary cookies',
+		purpose: `These are cookies that are required for the operation of
+our website. They include, for example, cookies that
+enable you to log into secure areas of our website, use a
+shopping cart or make use of e-billing services.`,
+	},
+	{
+		id: 2,
+		type: 'Analytical/performance cookies',
+		purpose: `They allow us to recognise and count the number of
+visitors and to see how visitors move around our website
+when they are using it. This helps us to improve the way
+our website works, for example, by ensuring that users
+are finding what they are looking for easily.`,
+	},
+	{
+		id: 3,
+		type: 'Functionality cookies',
+		purpose: `These are used to recognise you when you return to our
+website. This enables us to personalise our content for
+you, greet you by name and remember your preferences
+(for example, your choice of language or region). By
+using the Website, you agree to our placement of
+functionality cookie.`,
+	},
+];
+
+export default cookies;
